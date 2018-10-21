6f1a9946ee305b5724064d6ac43db7980d6c1f89
constantinos <constantinos.n.cn@gmail.com>
2018-10-21 14:51:35 +0300

    Refactor player selection

--- js/playerSelection.js
+++ js/playerSelection.js
@@ -1,14 +1,14 @@
+'use strict';
+const container = document.querySelector('.container');
+const container1 = document.querySelector('.container1');
+const metrics = document.querySelector('.metrics');
+const h4 = document.createElement('h4');
+
 // IIFE
-const Game = (function(global){
-  'use strict';
-  const document = global.document;
-  const container = document.querySelector('.container');
-  const metrics = document.querySelector('.metrics');
+const Game = (function(){
 
-  // Listen for character click
-  container.addEventListener('click', function(event) {
+  function selection(event) {
     const charSelected = document.querySelector('.charSelected');
-
     // Add .charSelected class to character clicked
     if (event.target.classList.contains('playerChar')) {
       // Remove charSelected if another
@@ -18,15 +18,16 @@ const Game = (function(global){
       }
       event.target.classList.add('charSelected');
     }
-  })
+  }
 
-  // Show a warning message if no character is charSelected
-  function warning() {
-    const warning = document.createElement('h4');
-    const container1 = document.querySelector('.container1');
-    warning.textContent = 'Please select a character first!';
-    warning.style.color = 'red';
-    container1.insertAdjacentElement('afterEnd', warning);
+  // Listen for character click
+  container.addEventListener('click', selection)
+
+  // Show a warning message if no character is Selected
+  function warning(h4) {
+    h4.textContent = 'Please select a character first!';
+    h4.style.color = 'red';
+    container1.insertAdjacentElement('afterEnd', h4);
   }
 
   // Hide player selection modal, reveal metrics, and
@@ -43,7 +44,7 @@ const Game = (function(global){
     // If no character is selected show
     // a warning, else start the game
     if (charSelected === null) {
-      warning();
+      warning(h4);
     } else {
       hidePlayerSelection(metrics, charSelected);
     }
@@ -55,4 +56,4 @@ const Game = (function(global){
     start: start
   }
 
-})(this);
+})();
