22cd570858fc270c69bfefe48e3b33325a21371f
constantinos <constantinos.n.cn@gmail.com>
2018-10-21 14:59:58 +0300

    Refactor Metrics so that the add and reset functions are private

--- js/metrics.js
+++ js/metrics.js
@@ -4,20 +4,28 @@ const score = document.querySelector('.score');
 const Metrics = (function() {
   let points = 0;
 
-  function addPoints() {
+  function add() {
     console.log('add points');
     points += 10;
     score.textContent = `Score: ${points}`;
   }
 
-  function resetPoints() {
+  function reset() {
     console.log('reset points');
     points = 0;
     score.textContent = `Score: ${points}`;
   }
 
+  function addPoints() {
+    add();
+  }
+
+  function resetPoints() {
+    reset();
+  }
+
   return {
     addPoints: addPoints,
-    resetPoints
+    resetPoints: resetPoints
   }
 })();
